d8c6cc7d08e82376d7feadef308b3931f73d9828
aparnasuresh28 <84711961+aparnasuresh28@users.noreply.github.com>
2021-06-27 12:09:57 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,31 @@
+void swap_max(int a[], int l, int n )
+{
+  int i,temp=0,count=0,max=0;
+  temp=a[n];
+  
+
+   for(i=n+1;i<l;i++)
+     {
+       if(a[i]>temp)
+       {temp=a[i];
+        count=i; 
+       
+      }
+          else continue;
+                 
+}
+if(temp>a[n])
+{
+a[count]=a[n];
+a[n]=temp;
+}
+}
+void ssort(int a[], int l)
+{
+    int i;
+     for(i=0;i<l;++i)
+     swap_max(a,l,i);
+     
+    
+       
+ }
